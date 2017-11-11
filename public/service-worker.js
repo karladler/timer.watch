d505213cebb32eec66fb53deb1392839cb069541
Karl <adlerkarl@gmail.com>
2017-11-11 23:39:13 +0100

    update cache when online for service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,44 +1,31 @@
 var CACHE_NAME = 'static-cache';
 
 var urlsToCache = [
-  '.',
-  './index.html',
-  './manifest.json'
+    '.',
+    './index.html',
+    './manifest.json'
 ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', function(event) {
+// fetch and update for next time
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-      caches.match(event.request)
-      .then(function(response) {
-        return response || fetchAndCache(event.request);
-      })
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                var fetchPromise = fetch(event.request).then(function (networkResponse) {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                })
+                return response || fetchPromise;
+            })
+        })
     );
-  });
-
-function fetchAndCache(url) {
-    return fetch(url)
-    .then(function(response) {
-      // Check if we received a valid response
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return caches.open(CACHE_NAME)
-      .then(function(cache) {
-        cache.put(url, response.clone());
-        return response;
-      });
-    })
-    .catch(function(error) {
-      console.log('Request failed:', error);
-      // You could return a custom offline 404 page here
-    });
-  }
+});
